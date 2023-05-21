96ab53caec1c384538a22747f4f5be7ce26b9fc6
nihal <nihal1481@tnraocollege.org>
2023-05-22 00:03:02 +0530

    this is js file of one.html

---
+++ try.js
@@ -0,0 +1,70 @@
+// inpage javascript <head tag > or <body tag> ma gme tya lahi saki.....<script>....</script> tag ma javascript no code lakhine
+
+
+
+// External javascript file banavin
+// <script src="try.js"></script> tag no use krine....aa tag end-body tag ni upr lakahai orrrrrrrr <head> tag ma pn lakhai...
+
+
+document.write("Hello India");
+
+
+
+document.write("</br><b><i>Hello World</i></b>");// Html tag used in js script
+
+// single line Comment
+// MultiLine COmment
+
+
+
+//variables
+// var
+// let
+// const
+
+var x = "hello Nihu";
+var y = "Hello Nibi...";
+var z = 100;
+z = "Nihal..."; // z ni aagad ek var VAR lakhaya pchi dar-vakhate lakhavani jaruur nthi ek var j declare kri gme tetli var usee kri ski
+z = "VNR";   // Z ni valyu ne over-right kri....means last ma j valyu assigne thai e print thai......... 
+
+var s= 555; // Numeric value ne quata "" ma no lakhai..
+
+// Boolean value ne quata ma no lakhai....
+
+document.write(x);
+document.write(y);
+document.write(z);
+document.write(s);
+
+
+
+// variable diclare ane value-assign alag-alag line ma karava... 
+
+var first,second;
+first=100;
+second="hello";
+document.write(first);
+document.write(second);
+document.write("</br>");
+document.write(typeof first);
+document.write("</br>");
+
+document.write(typeof second);
+
+// typeof function 6 e type batade variable ni...
+
+
+// let variable value ne over-right kre pn bigi vakaht ek j variable declare no karava dye means ek j var variale declare krva dye pn valyu mukltiple times assign krva dye......
+
+
+// aa  ek j diffrent hai VAR ane LET ma 
+
+
+// const ma ek j vakhat variable ne declare kri ski ane ek j vakhat value assign kri ski....
+
+
+
+//  Types of value is datatype
+
+// Variable Case-sensetive hoi----declare ane assign krava time ....
